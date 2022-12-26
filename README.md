3c0930f9779e1a367b9fe74f773e3b34b0fdc3ed
Joel Milligan <joelsaltwater@gmail.com>
2022-12-26 09:50:17 +0100

    Docker and compose support

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## TODO
 
 - [x] Setup PG/Sequelize
-- [ ] Docker/compose
+- [x] Docker/compose
 - [ ] Bun
 - [ ] Comments
 - [ ] Threaded comments
@@ -11,20 +11,26 @@
 - [ ] Linting
 
 # Setup
+
 To run, the following commands will start the application and dependencies:
+
 ```
 npm run pg
 npm run start:dev
 ```
 
 ## Migrations
+
 Typically with a fresh db, you will need to run the migrations first:
+
 ```
 npm run db:migrate
 ```
 
 ## Seeding the Database
+
 Must be done after migrations
+
 ```
 npm run db:seed
 ```
--- src/models/config.js
+++ src/models/config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    development: {
-        username: process.env.DB_USERNAME || "root",
-        password: process.env.DB_PASSWORD || "root",
-        database: process.env.DB_NAME || "",
-        host: process.env.DB_HOST || "localhost",
-        dialect: "postgres",
-      },
-}
+  development: {
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "",
+    host: process.env.DB_HOST || "localhost",
+    dialect: "postgres",
+  },
+};
